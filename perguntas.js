2023db024321b14bc6cc4a5e6d545deadbc27bdc
Isinha006 <isabellezamparoaparecido@gmail.com>
2024-11-25 10:01:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'terror slasher',
+    'O que é um filme slasher?',
+    'subgenêro do terror com psicopatas que matam aleatoriamente'
+)
+
+criaCartao(
+    'terror slasher',
+    'Jason Voorheers é o assassino de qual franquia?',
+    'Sexta-feira 13'
+)
+
+criaCartao(
+    'terror slasher',
+    'quem são os assassinos do panico de 1996?',
+    'Billy Loomis e Stu Macher'
+)
+
+criaCartao(
+    'terror slasher',
+    'Qual fime foi percursor do genero slasher?',
+    ' a tortura do medo'
+)
